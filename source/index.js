80b5ac152999bb95a260792ddf43801242064ad7
userfromhuy <userfromhuy@fedora.local>
2025-05-12 01:34:17 +0500

    the help card is finished

--- source/index.js
+++ source/index.js
@@ -1,12 +1,6 @@
 import { handleMobileBtn } from "./JavaScript/burger-menu";
 import { setImage } from "./JavaScript/image";
 
-import about_pets from "../public/images/about/about-pets.png";
-
-import katrine from "../public/images/slider/katrine.jpg";
-import jennifer from "../public/images/slider/jennifer.jpg";
-import woody from "../public/images/slider/woody.jpg";
-
 import "./styles/normalize.scss";
 import "./styles/style.scss";
 import "./styles/actions.scss";
@@ -17,6 +11,8 @@ import "./styles/block/nav.scss";
 import "./styles/block/main/about.scss";
 import "./styles/block/main/pet.scss";
 
+import "./styles/block/main/help.scss";
+
 const mobileBtn = window.matchMedia("(max-width: 500px)");
 
 // mobile button, hidden nav links
@@ -24,10 +20,23 @@ if (mobileBtn.matches) {
   handleMobileBtn(mobileBtn);
 }
 
-// download img
+// about block
+import about_pets from "../public/images/about/about-pets.png";
 setImage("about-pets", about_pets);
-console.log(about_pets);
+
+//slider block
+import katrine from "../public/images/slider/katrine.jpg";
+import jennifer from "../public/images/slider/jennifer.jpg";
+import woody from "../public/images/slider/woody.jpg";
 
 setImage("katrine", katrine);
 setImage("jennifer", jennifer);
 setImage("woody", woody);
+
+//help block
+const svgs = require.context("../public/images/help", false, /\.svg$/);
+
+for (let i = 1; i < 10; i++) {
+  const img = svgs(`./${i}.svg`);
+  setImage(`svg-${i}`, img);
+}
